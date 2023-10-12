85a746824b75679ac45eb87f0dc1ae0e78e71003
jspark474 <jspark474@gmail.com>
2023-10-11 21:08:50 -0400

    Coroutine

--- MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
+++ MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    //Coroutine co;
     protected virtual void Init()
     {
         base.Init();
@@ -11,12 +12,21 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        //co =  StartCoroutine("ExplodeAfterSeconds", 4f);
+        //StopCoroutine(co);
     }
+   
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Execute");
+    }
+
     public override void Clear()
     {
 
     }
-
-
 
 }
